6cc5fab030421608a7ac1a67bdc4dcb9e20be4c5
vazovn <n.a.vazov@usit.uio.no>
2026-05-18 15:40:56 +0200

    Improved preprocessing

--- 1_copy_tables_to_displomas-folders.sh
+++ 1_copy_tables_to_displomas-folders.sh
@@ -1,13 +1,29 @@
 #! /bin/bash
 
+## Copy the tables filled in by the teachers from Google Suite
+## to the respective three directories diplomas-bgschool-{present,distant,stavanger}
+## for processing. The directory from Google Suite shall have the name Подготовка-за-дипломите-$year/diplomas-bgschool-$eduform-teachers
+## where $year is the school year and $eduform is present, distant or stavanger
 
-## Copy the filled in  tables from Google Suite into the three respective folders stavanger, distant and present in Ready-Tables/
-## Change years every year by creating a separate folder for downloaded tables
-## Then run _THIS SCRIPT_ to copy the respective downloaded tables to the correct emplacements
+## Pull from Google Disk
+FOLDER_URL="https://drive.google.com/drive/folders/1IV3HPtcDKJYTiNrzzgEuez5UP--n_1v_"
+gdown --folder "$FOLDER_URL"
 
-for i in {1..11}; do cp ./Ready-Tables/present/$i-grade-data-table-word.docx diplomas-bgschool-present/$i-grade/; done
+## Process
+read -p "Enter year: " year
 
-for i in {1..11}; do cp ./Ready-Tables/distant/$i-grade-data-table-word.docx diplomas-bgschool-distant/$i-grade/; done
-
-for i in {1..11}; do cp ./Ready-Tables/stavanger/$i-grade-data-table-word.docx diplomas-bgschool-stavanger/$i-grade/; done
-
+for eduform in present distant stavanger; do
+    for i in {1..12}; do
+       if [[ -f "Подготовка-за-дипломите-$year/diplomas-bgschool-$eduform-teachers/$i-grade/$i-grade-data-table-word.docx" ]] ; then
+          echo "There is a class $eduform-$i-grade"
+          if [[ ! -d diplomas-bgschool-$eduform/$i-grade/ ]] ; then
+             mkdir diplomas-bgschool-$eduform/$i-grade/
+             echo "The directory for $eduform-$i-grade was missing. Created now!!"
+          fi
+          cp \
+          "./Подготовка-за-дипломите-$year/diplomas-bgschool-$eduform-teachers/$i-grade/$i-grade-data-table-word.docx" \
+          "./diplomas-bgschool-$eduform/$i-grade/"
+          echo "Copied $i-grade-data-table-word.docx to diplomas-bgschool-$eduform/$i-grade/"
+       fi
+    done
+done
